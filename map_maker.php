39e5ef60fdc30f344f2ed6d3587a88237b5bd20c
Jeff Switzer <jeff.switzer@gmail.com>
2020-01-28 10:58:25 -0700

    2020 anom animation

--- map_maker.php
+++ map_maker.php
@@ -79,6 +79,21 @@ class MapMaker{
         $this->generateMaps($arr);
         
     }
+
+    // public function forTheresa(){
+        
+    //     $arr = array(
+    //         /*
+    //         * For these pest layers, the parameter you can set to true
+    //         * indicates if it's for current day or 6 day forecast. Since
+    //         * we want both, we need to create two instances of each layer.
+    //         */
+                
+    //         new WinterMothLayer()
+    //     );
+
+    //     $this->generateMaps($arr);
+    // }
     
     public function runDailyPestMaps(){
         
@@ -188,9 +203,10 @@ class MapMaker{
 
     public function runDailyBuffelgrassAnimation() {
         $layers = [];
+        $current_year = date("Y");
         $startDate = date("Y-m-d", strtotime("6 months ago"));
         $endDate = date("Y-m-d", strtotime("now"));
-        $bgDates = $this->getDatesFromRange('2019-01-01', $endDate);
+        $bgDates = $this->getDatesFromRange($current_year . '-01-01', $endDate);
         // $bgDates = $this->getDatesFromRange($startDate, $endDate);
         foreach($bgDates as $bgDate) {
             array_push($layers, new BuffelgrassLayer(new DateTime($bgDate)));
@@ -200,7 +216,7 @@ class MapMaker{
         $previousWeekImage = OUTPUT_PATH . "buffelgrass-" . date("Y-m-d", strtotime("7 days ago")) . ".png";
         copy($currentDayImage, OUTPUT_PATH . "current-day-forecast-buffelgrass.png");
         copy($previousWeekImage, OUTPUT_PATH . "previous-week-forecast-buffelgrass.png");
-        exec('convert -limit memory 256MiB -delay 25 -loop 0 -layers optimize ' . OUTPUT_PATH . 'buffelgrass*.png ' . OUTPUT_PATH . 'buffelgrass.gif');
+        exec('convert -limit memory 256MiB -delay 25 -loop 0 -layers optimize ' . OUTPUT_PATH . 'buffelgrass-' . $current_year . '*.png ' . OUTPUT_PATH . 'buffelgrass.gif');
     }
 
     public function runDailySixAnomalyAnimation() {
@@ -215,8 +231,9 @@ class MapMaker{
             array_push($layers, new SpringIndexBloomAnomalyLayer(new DateTime($sixDate)));
         }
         $this->generateMaps($layers);
-        exec('convert -limit memory 256MiB -delay 25 -loop 0 -layers optimize ' . OUTPUT_PATH . 'six-leaf-index-anomaly-2019*.png ' . OUTPUT_PATH . 'six-leaf-index-daily-anomaly.gif');
-        exec('convert -limit memory 256MiB -delay 25 -loop 0 -layers optimize ' . OUTPUT_PATH . 'six-bloom-index-anomaly-2019*.png ' . OUTPUT_PATH . 'six-bloom-index-daily-anomaly.gif');
+        $current_year = date("Y");
+        exec('convert -limit memory 256MiB -delay 25 -loop 0 -layers optimize ' . OUTPUT_PATH . 'six-leaf-index-anomaly-' . $current_year . '*.png ' . OUTPUT_PATH . 'six-leaf-index-daily-anomaly-' . $current_year . '.gif');
+        exec('convert -limit memory 256MiB -delay 25 -loop 0 -layers optimize ' . OUTPUT_PATH . 'six-bloom-index-anomaly-' . $current_year . '*.png ' . OUTPUT_PATH . 'six-bloom-index-daily-anomaly-' . $current_year . '.gif');
     }
 
     public function runYearlySix(){
