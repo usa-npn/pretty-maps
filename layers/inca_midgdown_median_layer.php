7a9546224e5235eb6d7854961551c7b49d219b3f
npnlee85 <lee@usanpn.org>
2020-02-24 08:37:44 -0700

    Fixing INCA URLs

--- layers/inca_midgdown_median_layer.php
+++ layers/inca_midgdown_median_layer.php
@@ -18,7 +18,7 @@ class IncaMidgdownMedianLayer extends IncaLayer{
             $legend_x_start = 980;
             $legend_y_start = 760;
             
-            $curl_url = "https://geoserver-dev.usanpn.org/geoserver/wms?service=WMS&request=GetMap&bbox=-14000000,2700000,-7000000,6450000&srs=EPSG:3857&layers=inca:midgdown_median_nad83_02deg&width=1500&height=800&format=image/png&transparent=true";
+            $curl_url = "https://geoserver.usanpn.org/geoserver/wms?service=WMS&request=GetMap&bbox=-14000000,2700000,-7000000,6450000&srs=EPSG:3857&layers=inca:midgdown_median_nad83_02deg&width=1500&height=800&format=image/png&transparent=true";
 
             parent::__construct($title, $legend_width, $legend_height, $legend_x_start, $legend_y_start, $output_path, $thumb_output_path, $curl_url);
             
