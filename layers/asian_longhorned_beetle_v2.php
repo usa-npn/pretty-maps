c16ca912daf3e56ec2889c3210baff6bc38c89cc
Jeff Switzer <jeff.switzer@gmail.com>
2024-05-09 10:07:11 -0700

    alb-v2-legend

--- layers/asian_longhorned_beetle_v2.php
+++ layers/asian_longhorned_beetle_v2.php
@@ -10,8 +10,8 @@ class AsianLonghornedBeetleLayerV2 extends PestLayer{
     public function __construct($long_forecast=false){
         $title = "Asian Longhorned Beetle Forecast";
         
-        $legend_width = 350;
-        $legend_height= 127;
+        $legend_width = 300;
+        $legend_height= 217;
         
         $legend_x_start = 1145;
         $legend_y_start = 610;
@@ -22,7 +22,7 @@ class AsianLonghornedBeetleLayerV2 extends PestLayer{
         
         parent::__construct($title, $legend_width, $legend_height, $legend_x_start, $legend_y_start, $output_path, $curl_url, $long_forecast);
         $this->subtitle = "Base: 50°F, Upper: 86°F, Start: Jan 1";
-        $this->legend_path = $this->base_legend_path . "legend-asian-longhorned-beetle" . $this->extension;
+        $this->legend_path = $this->base_legend_path . "legend-alb-v2" . $this->extension;
         $this->overlay_path = $this->base_overlay_path . "overlay-asian-longhorned-beetle.png";   
  
     }
