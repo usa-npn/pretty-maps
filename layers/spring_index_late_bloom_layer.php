bc1f803c93d3033a5f8e3cf007cdf868365f3d3c
Jeff Switzer <jeff.switzer@gmail.com>
2025-05-22 16:29:37 -0700

    late bloom added

---
+++ layers/spring_index_late_bloom_layer.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('basic_layer.php');
+
+class SpringIndexLateBloomLayer extends BasicLayer{
+	
+	
+	public function __construct(){
+            $title = "Spring Late Bloom Index, " . date('F j, Y');
+            
+            $legend_width = 784;
+            $legend_height=40;
+            
+            $legend_x_start = 700;
+            $legend_y_start = 760;
+            
+            $output_path = 'six-late-bloom-index.png';
+            $curl_url = "http://geoserver.usanpn.org/geoserver/wms?service=WMS&request=GetMap&bbox=-14000000,2700000,-7000000,6450000&srs=EPSG:3857&layers=si-x:six_late_bloom_prism_2025&width=1500&height=800&time=" . date_format(new DateTime(), "Y-m-d") . "&format=image/png&transparent=true";
+
+            parent::__construct($title, $legend_width, $legend_height, $legend_x_start, $legend_y_start, $output_path,$curl_url);
+            
+            $this->legend_path = $this->base_legend_path . "legend-six" . $this->extension;
+            $this->overlay_path = $this->base_overlay_path . "current-spring-index-bloom.png";
+	}
+}
